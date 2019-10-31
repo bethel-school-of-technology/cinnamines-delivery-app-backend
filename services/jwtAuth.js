286004779745c9cb2e71ffe0d5c4085ad5b7a76a
cb <futurenz@gmail.com>
2019-11-01 06:18:18 +1300

    JWT service, install jsonwebtoken libs

---
+++ services/jwtAuth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const models = require('../models/user');
+
+var authService = {
+  signUser: function(user) {
+    const token = jwt.sign(
+      {
+        email: user.email,              // to connect with the model.
+        _id: user._id                   // should this be the ._id ???
+      },
+      'secretkey',                      // this is the "Secret Key" at the moment.
+      {
+        expiresIn: 60       // numeric values are in seconds. A numeric string will be in ms. Strings ie "1h" = 1 hour, "1d" = 1 day...
+      }
+    );
+    return token;
+  },
+
+  verifyUser: function (token) {  //<--- receive JWT token as parameter
+  try {
+    let decoded = jwt.verify(token, 'secretkey'); //<--- Decrypt token using same key used to encrypt
+    return models.users.findByPk(decoded._id); //<--- Return result of database query as promise
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
+
+}
+
+module.exports = authService;
